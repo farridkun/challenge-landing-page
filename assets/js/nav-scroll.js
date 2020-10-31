77eb8f742209e1357c70f1440f180f4c803ea6e4
farridkun <farridkuntoro@mahasiswa.unikom.ac.id>
2020-11-01 02:39:21 +0700

    New adding scroll nav simply

---
+++ assets/js/nav-scroll.js
@@ -0,0 +1,16 @@
+$(document).ready(() => {
+    $("a").on('click', function(event) {
+
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            let hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
